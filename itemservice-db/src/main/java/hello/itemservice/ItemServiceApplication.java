0cbf5f6a2002cc3f44dabccc77a9ed478ed5e269
marulog <gr214777@naver.com>
2025-10-21 19:30:46 +0900

    Add: JdbcTemplate_ver2

---
+++ itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -0,0 +1,29 @@
+package hello.itemservice;
+
+import hello.itemservice.config.*;
+import hello.itemservice.repository.ItemRepository;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
+
+
+//@Import(MemoryConfig.class) // 설정 파일로 사용
+//@Import(JdbcTemplateV1Config.class)
+@Import(JdbcTemplateV2Config.class)
+@SpringBootApplication(scanBasePackages = "hello.itemservice.web") // 해당 범위에서만 스캔 진행, 나머지는 수동 빈 등록
+public class ItemServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemServiceApplication.class, args);
+	}
+
+	@Bean
+	@Profile("local")// 프로필이 local일 때만 해당 객체 스프링 빈으로 등록
+	// 등록되면 init()실행으로 데이터 자동 등록
+	public TestDataInit testDataInit(ItemRepository itemRepository) {
+		return new TestDataInit(itemRepository);
+	}
+
+}
